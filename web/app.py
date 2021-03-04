82fa7698e7c89d1baae2038cec6e5a5b1c9a1382
Philipp Moser <info@philipp-moser.de>
2021-03-04 09:50:46 +0100

    Connect App to MongoDb

--- web/app.py
+++ web/app.py
@@ -1,8 +1,12 @@
 from flask import Flask, jsonify, request
 from flask_restful import Api, Resource
+from pymongo import MongoClient
 
 app = Flask(__name__)
 api = Api(app)
+
+client = MongoClient("mongodb://db:27017")
+db = client.MoneyManagementDB
 
 
 
