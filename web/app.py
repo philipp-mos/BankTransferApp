328eede2f03cfbcf37b15d5ec929fa18972afa30
Philipp Moser <info@philipp-moser.de>
2021-03-04 09:44:39 +0100

    Setup main app.py

---
+++ web/app.py
@@ -0,0 +1,10 @@
+from flask import Flask, jsonify, request
+from flask_restful import Api, Resource
+
+app = Flask(__name__)
+api = Api(app)
+
+
+
+if __name__=="__main__":
+    app.run(host='0.0.0.0')
